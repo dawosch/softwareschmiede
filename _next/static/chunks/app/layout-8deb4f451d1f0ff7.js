5e37f34219251d61928ce2432741a3cb7308b517
Dawosch <57100280+dawosch@users.noreply.github.com>
2025-01-03 21:54:28 +0100

    Updates

---
+++ _next/static/chunks/app/layout-8deb4f451d1f0ff7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2790:(e,a,l)=>{Promise.resolve().then(l.t.bind(l,9150,23)),Promise.resolve().then(l.t.bind(l,347,23))},347:()=>{},9150:e=>{e.exports={style:{fontFamily:"'Cairo Play', 'Cairo Play Fallback'",fontStyle:"normal"},className:"__className_376906"}}},e=>{var a=a=>e(e.s=a);e.O(0,[686,441,517,358],()=>a(2790)),_N_E=e.O()}]);
